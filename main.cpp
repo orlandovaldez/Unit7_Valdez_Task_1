d16f1a65bfa119ab9da0b63097ca16ded4937f06
orlandovaldez <orlando.valdez@my.tccd.edu>
2020-02-25 01:40:36 +0000

    declared structs

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Unit7_Valdez_Task_1 DR_T 2-24-2020
 //Orlando Valdez
 // COSC 1436
 // 2-24-20
@@ -5,10 +6,33 @@
 #include <iostream>
 
 using namespace std;
+//declare structs above int main
+
+struct timeOfDay
+{
+  short Hour;
+  short Minute;
+  short Second;
+};
+
+struct appDate
+{
+  short dayOfMonth;
+  short month;
+  int year;
+};
+
+struct event
+{
+  string eventName;
+  timeOfDay eventTime;
+  bool isUrgent;
+};
+
 
 int main()
 {
-
+  
 
 
   return 0;
